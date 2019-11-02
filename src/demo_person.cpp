2157eee4b59f7362911af9ae6aa5f3ea1ad2e5b5
Robertas Norkus <robertas.nork@gmail.com>
2019-11-02 15:33:29 +0000

    Implement Person

---
+++ src/demo_person.cpp
@@ -0,0 +1,38 @@
+/*
+ * Authors : Robertas Norkus
+ * Organisation: HYPED
+ * Date: 2. November 2019
+ * Description:
+ * This is the main executable for BeagleBone pod node
+ *
+ *    Copyright 2019 HYPED
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+#include <iostream>
+#include "things/person.hpp"
+
+using workshop5::things::Person;
+
+int main()
+{
+  //create new Person
+  Person person_("Robertas", 42, 199);
+
+  //output Person's data
+  std::cout << "Person's name: " << person_.getName() << "\n";
+  std::cout << "Person's age: " << person_.getAge() << "\n";
+  std::cout << "Person's height: " << person_.getHeight() << "\n";
+
+  return 0;
+}
