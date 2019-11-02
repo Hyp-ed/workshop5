d755e88b4f6fdb0bafbadac09364a926df32caa7
elisa-camero <elisa.camerorosero@gmail.com>
2019-11-02 15:57:43 +0000

    implement mean array

---
+++ src/array_mean.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+double meanArray(int a[])
+{
+    int length = sizeof(a);
+    double sum = 0;
+    
+    for(int x = 0; x++; x < length){
+        sum += a[x];
+    }
+
+    return sum/length;
+}
+
