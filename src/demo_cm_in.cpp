d88562e67c2e672ffa3a4a6831997edb534e8c0f
Laura Masselos <masselos.laura@gmail.com>
2019-11-02 16:16:52 +0000

    Update src/demo_cm_in.cpp
    Co-Authored-By: Paul <30511475+mazz3@users.noreply.github.com>

--- src/demo_cm_in.cpp
+++ src/demo_cm_in.cpp
@@ -6,7 +6,7 @@ using workshop5::utils::unit_conversions::cmToInches;
 int main()
 {
   double cm = 30;
-  printf("%f cm = %f inches\n", c, cmToInches(c));
+  printf("%f cm = %f inches\n", cm, cmToInches(cm));
 
   return 0;
 }
