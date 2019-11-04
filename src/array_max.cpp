7d87a8b0cf1767eb625be425b9bd9ea0cd90e86a
Yeyao Liu <56370494+Videosquared@users.noreply.github.com>
2019-11-04 17:09:51 +0000

    Update array_max.cpp

--- src/array_max.cpp
+++ src/array_max.cpp
@@ -10,10 +10,10 @@ int main() {
 int loop = 0;
 maxInt = array1[0];
 while (loop < sizeof(array1)+1) { 
-   if (array1[loop] > maxInt) {
-   maxInt = array1[loop];
-   }
-   loop++;
+  if (array1[loop] > maxInt) {
+  maxInt = array1[loop];
+  }
+  loop++;
 }
 
 return maxInt;  
